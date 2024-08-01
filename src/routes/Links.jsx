6f1f3436e638a879d14eff93db013915624ba76f
caitlinanderson099 <caitlin.anderson099@gmail.com>
2024-08-02 10:25:24 +1200

    first commit, set up base code

---
+++ src/routes/Links.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+// import the axios it is installed
+
+const Links = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Links
