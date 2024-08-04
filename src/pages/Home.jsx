da53b18e841847d724707930e58cf97f5506dfda
caitlinanderson099 <caitlin.anderson099@gmail.com>
2024-08-05 09:13:29 +1200

    started out the layout for the home page and created the context files with hooks and added the api key and the env file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      home page
-    </div>
+    <>
+      <div id='home-content-container'>
+        this is the home content container
+
+        <div id='featured-article'>
+          this is the featured article placeholder
+        </div>
+
+        <div id='article-grid'>
+            <div className='article-card'>
+              article card placeholder
+            </div>
+
+            <div className='article-card'>
+              article card placeholder
+            </div>
+
+            <div className='article-card'>
+              article card placeholder
+            </div>
+
+            <div className='article-card'>
+              article card placeholder
+            </div>
+        </div>
+      </div>
+    </>
   )
 }
 
