77479a33cee01dcc075942e0c3793429e7ab3fed
caitlinanderson099 <caitlin.anderson099@gmail.com>
2024-08-07 15:50:20 +1200

    started working on the page, added in selfie photo and project mockup photo

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,12 +6,18 @@ const About = () => {
       <div id='about-page-container'>
         <div id='about-page-content'>
 
-          <div id='about-me-section'>
-              about me
+          <div id='about-me-card'>
+              <img src='/pic-of-me.jpg' alt='picture of myself'/>
+              <h2> Caitlin Anderson </h2>
+              <h3> Digital Design Student | 20yrs </h3>
+              <p> Here is a little paragraph about myself as a designer/person in general. </p>
           </div>
 
-          <div id='about-project-section'>
-            about project
+          <div id='about-project-card'>
+            <img src='/news-project-mockup.png'/>
+            <h2> News API Website </h2>
+            <h3> Formative 3.1 </h3>
+            <p> Description about this project, brief, tools, etc </p>
           </div>
         </div>
 
